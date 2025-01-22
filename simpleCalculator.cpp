94d2cd6249be8567757fe6477d5ab13ef12b327a
Osman Yasar <osman.yasar@tum.de>
2025-01-22 23:08:05 +0100

    Sum/Subt Functionality

--- simpleCalculator.cpp
+++ simpleCalculator.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
 
+double sum(double a, double b){
+    return a+b;
+}
+
+double subtract(double a, double b){
+    return a-b;
+}
+
 int main(){
     //initializing the numbers and the operator
     double a = 0;
     double b = 0;
     std::string oper;
-    std::cout << "Welcome to the Calculator!";
-    std::cout << "--------------------------";
-    std::cout << "Currently available operations are:";
+    std::cout << "Welcome to the Calculator!\n";
+    std::cout << "--------------------------\n";
+    std::cout << "Currently available operations are:\n";
 
     //Print out current operations here.
+    std::cout << "1: Summation: +\n";
+    std::cout << "2: Subtract: -\n";
+ 
 
-    std::cout << "Please enter the first numero";
+    std::cout << "Please enter the first numero\n";
     std::cin >> a;
-    std::cout << "Enter the operation por favor";
+    std::cout << "Enter the operation por favor\n";
     std::cin >> oper;
-    std::cout << "Enter the second number SVP";
+    std::cout << "Enter the second number SVP\n";
     std::cin >> b;
 
     double result = 0;
 
-    //uncomment this to add your operation
-    // if (oper == YOUR_OPERATION){
-    //     result = YOUR_FUNCTION(a,b);
-    // }
-    // elif(oper == YOUR_OTHER_OPERATION).........
 
+    if (oper == "+"){
+        result = sum(a,b);
+    }
+    else if(oper == "-"){
+        result = subtract(a,b);
+    }
 
-    std::cout << "Result to your operation:";
+    std::cout << "Result to your operation:\n";
     std::cout << a << " " << oper << " " << b << " = " << result;
+    std::cout << "\n";
 }
