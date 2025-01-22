9230eb695d8f5c7ac50b629d09a5b1e3fb2602f0
Osman Yasar <osman.yasar@tum.de>
2025-01-22 22:44:22 +0100

    first commit

---
+++ simpleCalculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(){
+    //initializing the numbers and the operator
+    double a = 0;
+    double b = 0;
+    std::string oper;
+    std::cout << "Welcome to the Calculator!";
+    std::cout << "--------------------------";
+    std::cout << "Currently available operations are:";
+
+    //Print out current operations here.
+
+    std::cout << "Please enter the first numero";
+    std::cin >> a;
+    std::cout << "Enter the operation por favor";
+    std::cin >> oper;
+    std::cout << "Enter the second number SVP";
+    std::cin >> b;
+
+    double result = 0;
+
+    //uncomment this to add your operation
+    // if (oper == YOUR_OPERATION){
+    //     result = YOUR_FUNCTION(a,b);
+    // }
+    // elif(oper == YOUR_OTHER_OPERATION).........
+
+
+    std::cout << "Result to your operation:";
+    std::cout << a << " " << oper << " " << b << " = " << result;
+}
